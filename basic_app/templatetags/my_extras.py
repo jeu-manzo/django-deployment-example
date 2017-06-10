6336276fe20390731fe73d3fc1e3c06db867945d
Jeu Manzo <jeunettemanzo@gmail.com>
2017-06-10 16:15:11 -0500

    test

---
+++ basic_app/templatetags/my_extras.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    This cuts out all values of "arg form the string!"
+    """
+    return value.raplace(arg,'')
+
+#register.filter('cut',cut)
